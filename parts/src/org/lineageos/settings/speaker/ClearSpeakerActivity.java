25faeb2c65a290edc9eb1134b9530882e836fed2
Peter Kalauskas <peskal@google.com>
2023-09-18 22:10:57 +0000

    cepheus: parts: Enable use_resource_processor for all sysui deps
    This will make the build faster
    
    Test: m checkbuild
    Bug: 295208392
    Change-Id: I0c1bd901429bbe3bf81c1530e156735f8637a96e

--- parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
+++ parts/src/org/lineageos/settings/speaker/ClearSpeakerActivity.java
@@ -19,7 +19,6 @@ package org.lineageos.settings.speaker;
 import android.os.Bundle;
 
 import com.android.settingslib.collapsingtoolbar.CollapsingToolbarBaseActivity;
-import com.android.settingslib.widget.R;
 
 public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
 
@@ -30,7 +29,7 @@ public class ClearSpeakerActivity extends CollapsingToolbarBaseActivity {
         super.onCreate(savedInstanceState);
 
         getFragmentManager().beginTransaction()
-                .add(R.id.content_frame, new ClearSpeakerFragment(), TAG)
+                .add(com.android.settingslib.collapsingtoolbar.R.id.content_frame, new ClearSpeakerFragment(), TAG)
                 .commit();
     }
 }
