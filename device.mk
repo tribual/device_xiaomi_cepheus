7bb78482654d1abfeadb1825225329494adc1a7e
Kelvin Zhang <zhangkelvin@google.com>
2024-05-28 09:19:54 -0700

    cepheus: Allow to access bootable/deprecated-ota targets
    sm8150-common is still using non-AB update.
    non-AB code will be moved to a separated namespace, add
    deprecated-ota to sm8150-common namespace list ahead of time to avoid
    breakage.
    
    Test: th
    Bug: 324360816
    Change-Id: I909064be36e8febe80492cf010eb6eac3d6bc33d
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: firebird11 <hbgassel@gmail.com>

--- device.mk
+++ device.mk
@@ -489,6 +489,7 @@ PRODUCT_PACKAGES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
+    bootable/deprecated-ota \
     hardware/google/interfaces \
     hardware/xiaomi
 
