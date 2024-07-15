0c0cc1168f6930b3187cbe60b658dabbf7e1f147
chematelegram <chfondia@gmail.com>
2024-07-15 17:59:34 +0200

    cepheus: Switch to clang 19

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,7 +118,7 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CONFIG := cepheus_defconfig
 TARGET_KERNEL_SOURCE := kernel/xiaomi/cepheus
-TARGET_KERNEL_CLANG_VERSION := r498229b
+TARGET_KERNEL_CLANG_VERSION := r530567
 KERNEL_TOOLCHAIN := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r498229b/bin
 KERNEL_SUPPORTS_LLVM_TOOLS := true
 TARGET_KERNEL_ADDITIONAL_FLAGS := AS=llvm-as AR=llvm-ar NM=llvm-nm OBJCOPY=llvm-objcopy OBJDUMP=llvm-objdump STRIP=llvm-strip LD=ld.lld
