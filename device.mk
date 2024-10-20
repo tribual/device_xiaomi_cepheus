2db23b2460361f1d6bcbf879e63e1a3353f20ceb
¨raystef66¨ <s.vanbarel@gmail.com>
2024-10-20 11:39:40 +0200

    cepheus: build libcrypto-33 * CANNOT LINK EXECUTABLE "/odm/bin/hw/android.hardware.drm@1.3-service.widevine": cannot locate symbol "CBS_init" referenced by "/vendor/lib64/libwvhidl.so"

--- device.mk
+++ device.mk
@@ -175,7 +175,8 @@ PRODUCT_PACKAGES += \
     android.hardware.drm@1.1.vendor \
     android.hardware.drm@1.2.vendor \
     android.hardware.drm@1.3.vendor \
-    android.hardware.drm-service.clearkey
+    android.hardware.drm-service.clearkey \
+    libcrypto-v33
 
 # Enable incremental FS feature
 PRODUCT_PROPERTY_OVERRIDES += ro.incremental.enable=1
