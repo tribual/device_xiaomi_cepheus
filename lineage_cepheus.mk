1dd564eef241536fe517641a16ddb40b241fdd4f
chematelegram <chfondia@gmail.com>
2024-12-19 21:38:16 +0100

    cepheus: Enforce Product Packages Exist again

--- lineage_cepheus.mk
+++ lineage_cepheus.mk
@@ -25,7 +25,6 @@ $(call inherit-product, device/xiaomi/cepheus/device.mk)
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 TARGET_BOOT_ANIMATION_RES := 1080
-TARGET_DISABLE_EPPE := true
 TARGET_HAS_UDFPS := true
 BUILD_SEEDVAULT := true
 TARGET_USES_MINI_GAPPS := true
