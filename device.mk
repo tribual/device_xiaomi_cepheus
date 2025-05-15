1f08c5c2cdda36918f85b6e412d4c755f2736667
New Author Name <bgcngm@gmail.com>
2025-05-15 10:56:30 +0200

    cepheus: Set camera override format from reserved via soong config

--- device.mk
+++ device.mk
@@ -324,9 +324,8 @@ PRODUCT_COPY_FILES += \
 
 # MiuiCamera
 TARGET_CAMERA_PACKAGE_NAME := com.android.camera
-TARGET_CAMERA_OVERRIDE_FORMAT_FROM_RESERVED := true
-TARGET_USES_MIUI_CAMERA := true
-$(call inherit-product-if-exists, vendor/xiaomi/miuicamera-cepheus/config.mk)
+$(call soong_config_set,camera,override_format_from_reserved,true)
+$(call inherit-product-if-exists, vendor/xiaomi/miuicamera/config.mk)
 
 # Network
 PRODUCT_PACKAGES += \
