0b07975cd00b5565c84e1e932d156e6fc8e5785c
¨raystef66¨ <s.vanbarel@gmail.com>
2025-08-26 16:27:18 +0200

    Revert "cepheus: Migrate to Xiaomi power HAL AIDL"
    This reverts commit 4f5ac015267d1dcc5f7476f40b2edafa31f8fb7e.

--- device.mk
+++ device.mk
@@ -402,7 +402,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr
+    android.hardware.power@1.3-service.xiaomi_msmnile
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/power-libperfmgr/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
@@ -490,7 +490,6 @@ PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
     bootable/deprecated-ota \
     hardware/google/interfaces \
-    hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client \
     hardware/xiaomi \
