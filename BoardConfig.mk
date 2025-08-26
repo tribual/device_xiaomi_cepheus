4c8b5c938f4ebf446d9830801bdc54851dfca9a8
¨raystef66¨ <s.vanbarel@gmail.com>
2025-08-26 16:23:04 +0200

    Revert "cepheus: Provide a static lib for power mode extension"
    This reverts commit 17357898c6c7df2d4e19b2be7bebe3d6b70533b4.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -173,7 +173,7 @@ TARGET_COPY_OUT_VENDOR := vendor
 TARGET_BOARD_PLATFORM := msmnile
 
 # Power
-TARGET_POWER_LIBPERFMGR_MODE_EXTENSION_LIB := //$(DEVICE_PATH):libperfmgr-ext-xiaomi
+TARGET_POWERHAL_MODE_EXT := $(DEVICE_PATH)/power/power-mode.cpp
 
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
