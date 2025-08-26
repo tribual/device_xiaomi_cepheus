f9186434a9dc6b3bf2d926406a2a4a23502b9f54
¨raystef66¨ <s.vanbarel@gmail.com>
2025-08-26 16:27:50 +0200

    Revert "cepheus: Import power AIDL extension for DT2W"
    This reverts commit 7b1cdb102f0914d8f9eab07e09e318711abfdf3b.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -172,9 +172,6 @@ TARGET_COPY_OUT_VENDOR := vendor
 # Platform
 TARGET_BOARD_PLATFORM := msmnile
 
-# Power
-TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/wake_gesture"
-
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/product.prop
